7b82b8ab3cf0b88bf499e3d667d0ec35d46f54bb
Kanika Singhal <kanika.singhal@siriusai.com>
2025-06-30 14:04:28 +0530

    Changes to deploy in render

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'dummy-for-dev')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
